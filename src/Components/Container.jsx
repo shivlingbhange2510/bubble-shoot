afe842fae8124a3ec505519536c80b67cb2ae2d1
shivlingbhange2510 <shivlingbhange2510@gmail.com>
2022-05-30 01:56:47 -0700

    add container add css

---
+++ src/Components/Container.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  conatinerToQueue,
+  removeFromContainer,
+} from "../redux/action";
+export const Container = () => {
+  const data = useSelector((store) => store.container);
+  const dispatch = useDispatch()
+  const removeContainer = (val) => {
+    const add = data.find((item, index) => item.id === +val);
+
+    dispatch(removeFromContainer(val));
+    dispatch(conatinerToQueue(add));
+ 
+  };
+  return (
+    <div>
+      conainer
+      <div id="container">
+        {data?.map((item, index) => {
+          return (
+            <div>
+              <div
+                key={index}
+                onClick={() => {
+                  removeContainer(item.id);
+                }}
+                className="circle"
+                style={{ backgroundColor: `${item?.color}` }}
+              ></div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
