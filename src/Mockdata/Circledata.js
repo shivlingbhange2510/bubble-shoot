503f42b167d68b6326c630e5d46dbb0243b9a86d
shivlingbhange2510 <shivlingbhange2510@gmail.com>
2022-05-30 00:11:05 -0700

    added mockdata

---
+++ src/Mockdata/Circledata.js
@@ -0,0 +1,28 @@
+
+export const circleData=[
+    {
+        id:1,
+        color:  '#'+Math.floor(Math.random()*16777215 +20).toString(16),
+        position:0,
+    },
+    {
+        id:2,
+        color:  '#'+Math.floor(Math.random()*16777215 + 10).toString(16),
+        position:2,
+    },
+    {
+        id:3,
+        color:  '#'+Math.floor(Math.random()*16777215 +30).toString(16),
+        position:0,
+    },
+    {
+        id:4,
+        color:  '#'+Math.floor(Math.random()*16777215 +40).toString(16),
+        position:0,
+    },
+    {
+        id:5,
+        color:  '#'+Math.floor(Math.random()*16777215 +50).toString(16),
+        position:0,
+    }
+]
